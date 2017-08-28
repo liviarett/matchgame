61f71b3b436472830760a0e1162db96e549bef86
Livia Rett <livia.rett@gmail.com>
2017-08-28 20:58:28 +0100

    first bit of js done

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -14,45 +14,36 @@ MatchGame.generateCardValues = function () {
     var orderedValues = [];
     
     for (var i = 1; i < 9; i++) {
-        orderedValues.push[i];
-        orderedValues.push[i];
+        orderedValues.push(i);
+        orderedValues.push(i);
+        console.log(orderedValues);
     }
-  
-    var randomValues = [];
     
-    while (var a <= orderedValues.length) {
-        
-        a = 0;
-        
+    var randomValues = [];
+    var a = 0;
+    var numberOfCards = orderedValues.length;
+    
+    while (a < numberOfCards) {
         var randomIndex = Math.floor(Math.random() * orderedValues.length);
-        
-        randomValues.push[orderedValues[randomIndex]];
-        
-        cardValues.splice(randomIndex, 1);
-        
+        randomValues.push(orderedValues[randomIndex]);
+        orderedValues.splice(randomIndex, 1);
         a++;
-        
     }
+    
      return randomValues;
     
      // Math.floor(Math.random() * (8 - 1 + 1)) + 1; // You could use Math.round(Math.random() * (max - min)) + min, this however gives a non-even distribution. Both, min and max only have approximately half the chance to roll:
 };
-
-
 /*
   Converts card values to jQuery card objects and adds them to the supplied game
   object.
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
-
 };
-
 /*
   Flips over a given card and checks to see if two cards are flipped over.
   Updates styles on flipped cards depending whether they are a match or not.
  */
-
 MatchGame.flipCard = function($card, $game) {
-
 };
