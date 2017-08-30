458db8dc2a1bbe7d0c3eae173d5c31fb743adb2c
Livia Rett <livia.rett@gmail.com>
2017-08-30 01:13:33 +0100

    main js done

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,3 +1,12 @@
+// Indicate that the user won when all pairs have been found
+/* Add a "Restart Game" button
+Only allow two cards to be visible at a time (currently the setTimeout allows users to click really quickly and see a few)
+Change card values to non-number values
+Add score or time
+Allow user to select from multiple board sizes
+Add sound effects
+Add flipping animations */
+
 $(document).ready(function() {
     MatchGame.generateCardValues();
     MatchGame.renderCards(cardValues, $('#game'));
